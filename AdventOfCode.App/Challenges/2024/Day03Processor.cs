590e46d32aa82f6e2ae33ff132598c19aae80861
Quangdao Nguyen <qnguyen@nvisia.com>
2024-12-04 08:44:26 -0600

    Move regex up

--- AdventOfCode.App/Challenges/2024/Day03Processor.cs
+++ AdventOfCode.App/Challenges/2024/Day03Processor.cs
@@ -6,6 +6,12 @@ namespace AdventOfCode.App.Challenges;
 [ChallengeProcessor(2024, 3)]
 public partial class Aoc2024Day03Processor : IChallengeProcessor
 {
+  [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
+  private static partial Regex Part1Regex();
+
+  [GeneratedRegex(@"(mul|do(?:n't)?)\((\d+(?:,\d+)+)?\)")]
+  private static partial Regex Part2Regex();
+  
   public string ProcessPart1Solution(string input)
   {
     var matches = Part1Regex().Matches(input);
@@ -51,10 +57,4 @@ public partial class Aoc2024Day03Processor : IChallengeProcessor
 
     return sum.ToString();
   }
-
-  [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
-  private static partial Regex Part1Regex();
-
-  [GeneratedRegex(@"(mul|do(?:n't)?)\((\d+(?:,\d+)+)?\)")]
-  private static partial Regex Part2Regex();
 }
