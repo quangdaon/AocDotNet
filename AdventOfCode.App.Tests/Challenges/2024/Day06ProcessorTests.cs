756f0a73d34660f32441cd64b9ba80375581118b
Quangdao Nguyen <qnguyen@nvisia.com>
2024-12-06 14:16:32 -0600

    2024 Day 6 Pre-jank

--- AdventOfCode.App.Tests/Challenges/2024/Day06ProcessorTests.cs
+++ AdventOfCode.App.Tests/Challenges/2024/Day06ProcessorTests.cs
@@ -60,9 +60,157 @@ public class Aoc2024Day06ProcessorTest : ChallengeProcessorTests
     ]);
 
     var result = Aoc2024Day06Processor.PatrolUp(mask, map, 3, 2);
-    
-    Assert.Equal(expected, map);
+
+    Assert.Equal(expected, mask);
     Assert.Equal(1, result);
+  }
+
+  [Fact]
+  public void PatrolUp_GivenMask_ReservesMask()
+  {
+    var map = CreateMap([
+      "00010",
+      "00000",
+      "00000"
+    ]);
+    var mask = CreateMap([
+      "00000",
+      "00111",
+      "00000"
+    ]);
+    var expected = CreateMap([
+      "00000",
+      "00111",
+      "00010"
+    ]);
+
+    var result = Aoc2024Day06Processor.PatrolUp(mask, map, 3, 2);
+
+    Assert.Equal(expected, mask);
+    Assert.Equal(1, result);
+  }
+
+  [Fact]
+  public void PatrolUp_GivenClearance_ReturnsNegativeOne()
+  {
+    var map = CreateMap([
+      "00000",
+      "00000",
+      "00000"
+    ]);
+    var mask = CreateMap([
+      "00000",
+      "00000",
+      "00000"
+    ]);
+    var expected = CreateMap([
+      "00010",
+      "00010",
+      "00000"
+    ]);
+
+    var result = Aoc2024Day06Processor.PatrolUp(mask, map, 3, 1);
+
+    Assert.Equal(expected, mask);
+    Assert.Equal(-1, result);
+  }
+
+  [Fact]
+  public void PatrolDown_GivenObstacle_Stops()
+  {
+    var map = CreateMap([
+      "00000",
+      "00000",
+      "00010"
+    ]);
+    var mask = CreateMap([
+      "00000",
+      "00000",
+      "00000"
+    ]);
+    var expected = CreateMap([
+      "00010",
+      "00010",
+      "00000",
+    ]);
+
+    var result = Aoc2024Day06Processor.PatrolDown(mask, map, 3, 0);
+
+    Assert.Equal(expected, mask);
+    Assert.Equal(1, result);
+  }
+
+  [Fact]
+  public void PatrolDown_GivenMask_ReservesMask()
+  {
+    var map = CreateMap([
+      "00000",
+      "00000",
+      "00010",
+    ]);
+    var mask = CreateMap([
+      "00000",
+      "00111",
+      "00000"
+    ]);
+    var expected = CreateMap([
+      "00010",
+      "00111",
+      "00000"
+    ]);
+
+    var result = Aoc2024Day06Processor.PatrolDown(mask, map, 3, 0);
+
+    Assert.Equal(expected, mask);
+    Assert.Equal(1, result);
+  }
+
+  [Fact]
+  public void PatrolDown_GivenClearance_ReturnsNegativeOne()
+  {
+    var map = CreateMap([
+      "00000",
+      "00000",
+      "00000"
+    ]);
+    var mask = CreateMap([
+      "00000",
+      "00000",
+      "00000"
+    ]);
+    var expected = CreateMap([
+      "00000",
+      "00010",
+      "00010"
+    ]);
+
+    var result = Aoc2024Day06Processor.PatrolDown(mask, map, 3, 1);
+
+    Assert.Equal(expected, mask);
+    Assert.Equal(-1, result);
+  }
+
+  [Theory]
+  [InlineData("00000000", "00000000", "11111111", 1, 0, -1)] // LTR No Obstacle
+  [InlineData("00000000", "00000000", "00011111", 1, 3, -1)] // LTR No Obstacle, Offset Start
+  [InlineData("00000100", "00000000", "11111000", 1, 0, 4)] // LTR Obstacle
+  [InlineData("00000100", "00000000", "00011000", 1, 3, 4)] // LTR Obstacle, Offset Start
+  [InlineData("00000100", "00000010", "11111010", 1, 0, 4)] // LTR Keep Existing Mask
+  [InlineData("00000000", "00000000", "11111111", -1, 7, -1)] // RTL No Obstacle
+  [InlineData("00000000", "00000000", "11110000", -1, 3, -1)] // RTL No Obstacle, Offset Start
+  [InlineData("00100000", "00000000", "00011111", -1, 7, 3)] // RTL Obstacle
+  [InlineData("00100000", "00000000", "00011100", -1, 5, 3)] // RTL Obstacle, Offset Start
+  [InlineData("00100000", "01000000", "01011111", -1, 7, 3)] // RTL Keep Existing Mask
+  public void PatrolHorizontal_GivenDirections_ExecutesExpectedBehavior(string mapRow, string maskRow, string expectedRow, int dir, int x, int expectedIndex)
+  {
+    var map = CreateMapRow(mapRow);
+    var mask = CreateMapRow(maskRow);
+    var expected = CreateMapRow(expectedRow);
+
+    var result = Aoc2024Day06Processor.PatrolHorizontal(dir, mask, map, x);
+
+    Assert.Equal(expected, mask);
+    Assert.Equal(expectedIndex, result);
   }
 
   [Fact]
@@ -86,8 +234,6 @@ public class Aoc2024Day06ProcessorTest : ChallengeProcessorTests
     Assert.Equal("???", result);
   }
 
-  private bool[][] CreateMap(string[] map)
-  {
-    return map.Select(e => e.ToCharArray().Select(c => c == '1').ToArray()).ToArray();
-  }
+  private bool[] CreateMapRow(string row) => row.ToCharArray().Select(c => c == '1').ToArray();
+  private bool[][] CreateMap(string[] map) => map.Select(CreateMapRow).ToArray();
 }
--- AdventOfCode.App/Challenges/2024/Day06Processor.cs
+++ AdventOfCode.App/Challenges/2024/Day06Processor.cs
@@ -38,31 +38,86 @@ public class Aoc2024Day06Processor : IChallengeProcessor
     var x = startX;
     var y = startY;
     var mask = map.Select(e => e.Select(f => false).ToArray()).ToArray();
+    var next = 0;
 
-    PatrolUp(mask, map, x, y);
+    while (next > -1)
+    {
+      next = dir switch
+      {
+        1 => PatrolUp(mask, map, x, y),
+        2 => PatrolHorizontal(1, mask[y], map[y], x),
+        3 => PatrolDown(mask, map, x, y),
+        4 => PatrolHorizontal(-1, mask[y], map[y], x),
+        _ => throw new InvalidOperationException("Shit's fucked.")
+      };
+
+      if (dir % 2 == 0)
+      {
+        x = next;
+      }
+      else
+      {
+        y = next;
+      }
+
+      dir += 1;
+      if (dir > 4) dir = 1;
+    }
+
     return mask;
+  }
+
+  public static int PatrolHorizontal(int dir, bool[] maskRow, bool[] mapRow, int x)
+  {
+    var i = x;
+    while ((dir == -1 && i >= 0) || (dir == 1 && i < mapRow.Length))
+    {
+      if (mapRow[i]) return i - dir;
+      maskRow[i] = true;
+      i += dir;
+    }
+
+    return -1;
   }
 
   public static int PatrolUp(bool[][] mask, bool[][] map, int x, int y)
   {
-    var targetY = Array.FindLastIndex(map.Take(y).ToArray(), m => m[x]);
+    var obstacleY = Array.FindLastIndex(map.Take(y).ToArray(), m => m[x]);
 
-    for (var i = y; i >= targetY; i--) mask[y][x] = true;
+    for (var i = y; i > obstacleY; i--) mask[i][x] = true;
 
-    return targetY;
+    return obstacleY > -1 ? obstacleY + 1 : -1;
+  }
+
+  public static int PatrolDown(bool[][] mask, bool[][] map, int x, int y)
+  {
+    var obstacleY = Array.FindIndex(map.Skip(y).ToArray(), m => m[x]);
+    var targetY = obstacleY > -1 ? obstacleY + y - 1 : map.Length - 1;
+
+    for (var i = y; i <= targetY; i++) mask[i][x] = true;
+
+    return obstacleY > -1 ? targetY : -1;
   }
 
   public string ProcessPart1Solution(string input)
   {
     var (map, startX, startY) = ParseInput(input);
-    
+
     var mask = Patrol(map, startX, startY);
+    
+    // PrintMap(map, mask);
 
     return mask.Sum(e => e.Count(c => c)).ToString();
   }
-
+  
   public string ProcessPart2Solution(string input)
   {
     throw new NotImplementedException();
+  }
+
+  // ReSharper disable once UnusedMember.Local
+  private static void PrintMap(bool[][] map, bool[][] mask)
+  {
+    foreach (var s in map.Select((m, y) => string.Join("", m.Select((n, x) => n ? '#' : mask[y][x] ? 'X' : '.')))) Console.WriteLine(s);
   }
 }
