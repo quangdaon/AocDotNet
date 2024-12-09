79ec6abfaccdccaa0fa46c4df8230f14e0c5da13
quangdaon <me@quangdao.com>
2024-12-09 08:15:08 -0600

    2024 Day 8

---
+++ AdventOfCode.App.Tests/Challenges/2024/Day08ProcessorTests.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using AdventOfCode.App.Challenges;
+using Xunit;
+using Coordinates = (int x, int y);
+
+namespace AdventOfCode.App.Tests.Challenges;
+
+public class Aoc2024Day08ProcessorTest : ChallengeProcessorTests
+{
+  private readonly Aoc2024Day08Processor _processor;
+
+  public Aoc2024Day08ProcessorTest() : base(2024, 8)
+  {
+    _processor = new Aoc2024Day08Processor();
+  }
+
+  [Fact]
+  public void GetAntinodes_GivenPair_ReturnsAntinodes()
+  {
+    var result = Aoc2024Day08Processor.GetAntinodes((4, 3), (5, 5), 10, 10);
+
+    Assert.Equal(2, result.Length);
+    Assert.Contains(result, x => x == (3, 1));
+    Assert.Contains(result, x => x == (6, 7));
+  }
+
+  [Fact]
+  public void GetUnboundedAntinodes_GivenPair_ReturnsAntinodes()
+  {
+    var result = Aoc2024Day08Processor.GetUnboundedAntinodes((0,0), (3,1), 10, 10);
+
+    Assert.Equal(4, result.Length);
+    Assert.Contains(result, x => x == (0, 0));
+    Assert.Contains(result, x => x == (3, 1));
+    Assert.Contains(result, x => x == (6, 2));
+    Assert.Contains(result, x => x == (9, 3));
+  }
+
+  [Fact]
+  public void GetAntinodes_GivenPair_IgnoresOutOfBoundAntinodes()
+  {
+    var result = Aoc2024Day08Processor.GetAntinodes((4, 3), (8, 4), 10, 10);
+
+    Assert.Single(result);
+    Assert.Contains(result, x => x == (0, 2));
+  }
+
+  [Fact]
+  public void GetAntinodes_GivenList_ReturnsAntinodes()
+  {
+    Coordinates[] nodes = [(4, 3), (5, 5), (8, 4)];
+    var result = Aoc2024Day08Processor.GetAntinodes(nodes, 10, 10);
+
+    Assert.Equal(4, result.Length);
+    Assert.Contains(result, x => x == (3, 1));
+    Assert.Contains(result, x => x == (6, 7));
+    Assert.Contains(result, x => x == (0, 2));
+    Assert.Contains(result, x => x == (2, 6));
+  }
+
+  [Fact]
+  public void GetNodes_GivenSampleInputs_ReturnsNodes()
+  {
+    var input = GetSampleInput();
+
+    var result = Aoc2024Day08Processor.GetNodes(input.Split(Environment.NewLine));
+
+    var expected = new Dictionary<char, Coordinates[]>
+    {
+      { '0', [(8, 1), (5, 2), (7, 3), (4, 4)] },
+      { 'A', [(6, 5), (8, 8), (9, 9)] }
+    };
+
+    Assert.Equal(expected, result);
+  }
+
+  [Fact]
+  public void ProcessPart1Solution_GivenSampleInputs_ReturnsProvidedResult()
+  {
+    var input = GetSampleInput();
+
+    var result = _processor.ProcessPart1Solution(input);
+
+    Assert.Equal("14", result);
+  }
+
+  [Fact]
+  public void ProcessPart2Solution_GivenSampleInputs_ReturnsProvidedResult()
+  {
+    var input = GetSampleInput();
+
+    var result = _processor.ProcessPart2Solution(input);
+
+    Assert.Equal("34", result);
+  }
+}
---
+++ AdventOfCode.App/Challenges/2024/Day08Processor.cs
@@ -0,0 +1,110 @@
+using AdventOfCode.App.Core;
+using Coordinates = (int x, int y);
+
+namespace AdventOfCode.App.Challenges;
+
+[ChallengeProcessor(2024, 8)]
+public class Aoc2024Day08Processor : IChallengeProcessor
+{
+  public static Dictionary<char, Coordinates[]> GetNodes(string[] input)
+  {
+    var nodes = new Dictionary<char, List<(int x, int y)>>();
+
+    for (var y = 0; y < input.Length; y++)
+    {
+      var row = input[y].ToCharArray();
+      for (var x = 0; x < row.Length; x++)
+      {
+        var node = row[x];
+        if (node == '.') continue;
+
+        if (!nodes.ContainsKey(node)) nodes.Add(node, []);
+
+        nodes[node].Add((x, y));
+      }
+    }
+
+    return nodes.ToDictionary(x => x.Key, x => x.Value.ToArray());
+  }
+
+  public static Coordinates[] GetUnboundedAntinodes(Coordinates node1, Coordinates node2, int width, int height)
+  {
+    Coordinates slope = (node2.x - node1.x, node2.y - node1.y);
+
+    List<Coordinates> antinodeCandidates = [node1, node2];
+
+    var tl = node1;
+
+    while (true)
+    {
+      tl = (tl.x - slope.x, tl.y - slope.y);
+      if (!IsInBound(tl, width, height)) break;
+
+      antinodeCandidates.Add(tl);
+    }
+
+    var br = node2;
+    while (true)
+    {
+      br = (br.x + slope.x, br.y + slope.y);
+      if (!IsInBound(br, width, height)) break;
+
+      antinodeCandidates.Add(br);
+    }
+
+    return antinodeCandidates.Where(e => IsInBound(e, width, height)).ToArray();
+  }
+
+  public static Coordinates[] GetAntinodes(Coordinates node1, Coordinates node2, int width, int height)
+  {
+    Coordinates slope = (node2.x - node1.x, node2.y - node1.y);
+
+    Coordinates[] antinodeCandidates =
+    [
+      (node1.x - slope.x, node1.y - slope.y),
+      (node2.x + slope.x, node2.y + slope.y),
+    ];
+
+    return antinodeCandidates.Where(e => IsInBound(e, width, height)).ToArray();
+  }
+
+  public static Coordinates[] GetAntinodes(Coordinates[] nodes, int width, int height, bool unbounded = false)
+  {
+    var pairs = nodes
+      .SelectMany((n1, i) => nodes
+        .Skip(i + 1)
+        .Select(n2 => new[] { n1, n2 }));
+
+    return pairs.SelectMany(pair => unbounded
+        ? GetUnboundedAntinodes(pair[0], pair[1], width, height)
+        : GetAntinodes(pair[0], pair[1], width, height))
+      .Distinct().ToArray();
+  }
+
+  public string ProcessPart1Solution(string input)
+  {
+    var rows = input.Split(Environment.NewLine);
+    var nodes = GetNodes(rows);
+    var antinodes = nodes.ToDictionary(n => n.Key, n => GetAntinodes(n.Value, rows.Length, rows[0].Length));
+
+    var allAntinodes = antinodes.SelectMany(a => a.Value).Distinct().ToArray();
+
+    return allAntinodes.Length.ToString();
+  }
+
+  public string ProcessPart2Solution(string input)
+  {
+    var rows = input.Split(Environment.NewLine);
+    var nodes = GetNodes(rows);
+    var antinodes = nodes.ToDictionary(n => n.Key, n => GetAntinodes(n.Value, rows.Length, rows[0].Length, true));
+
+    var allAntinodes = antinodes.SelectMany(a => a.Value).Distinct().ToArray();
+
+    return allAntinodes.Length.ToString();
+  }
+
+  private static bool IsInBound(Coordinates e, int width, int height)
+  {
+    return e.x >= 0 && e.x < width && e.y >= 0 && e.y < height;
+  }
+}
