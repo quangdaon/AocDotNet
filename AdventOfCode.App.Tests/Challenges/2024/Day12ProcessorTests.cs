678547ac151d93dada6ed45ba34f7e250241da05
Quangdao Nguyen <qnguyen@nvisia.com>
2024-12-12 13:07:08 -0600

    2024 Day 12 Part 2

--- AdventOfCode.App.Tests/Challenges/2024/Day12ProcessorTests.cs
+++ AdventOfCode.App.Tests/Challenges/2024/Day12ProcessorTests.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using AdventOfCode.App.Challenges;
 using AdventOfCode.App.Utilities;
 using Xunit;
+using Cell = (int X, int Y, AdventOfCode.App.Challenges.Border[] Perimeter);
 
 namespace AdventOfCode.App.Tests.Challenges;
 
@@ -32,7 +33,7 @@ public class Aoc2024Day12ProcessorTest : ChallengeProcessorTests
     var grid = map.ToCharGrid();
     var result = Aoc2024Day12Processor.GetPerimeter(grid, c, x, y);
 
-    Assert.Equal(expected, result);
+    Assert.Equal(expected, result.Length);
   }
 
   [Fact]
@@ -44,13 +45,13 @@ public class Aoc2024Day12ProcessorTest : ChallengeProcessorTests
     var result = Aoc2024Day12Processor.Crawl(grid, 'A', 0, 0);
     
     Assert.Equal(7, result.Count);
-    Assert.Contains((0, 0, 2), result);
-    Assert.Contains((1, 0, 1), result);
-    Assert.Contains((2, 0, 2), result);
-    Assert.Contains((0, 1, 2), result);
-    Assert.Contains((1, 1, 0), result);
-    Assert.Contains((2, 1, 2), result);
-    Assert.Contains((1, 2, 3), result);
+    Assert.Contains(result, e => e is { X: 0, Y: 0 });
+    Assert.Contains(result, e => e is { X: 1, Y: 0 });
+    Assert.Contains(result, e => e is { X: 2, Y: 0 });
+    Assert.Contains(result, e => e is { X: 0, Y: 1 });
+    Assert.Contains(result, e => e is { X: 1, Y: 1 });
+    Assert.Contains(result, e => e is { X: 2, Y: 1 });
+    Assert.Contains(result, e => e is { X: 1, Y: 2 });
   }
 
   [Fact]
@@ -67,11 +68,70 @@ public class Aoc2024Day12ProcessorTest : ChallengeProcessorTests
   [Fact]
   public void CalculateRegionValue_GivenRegion_ReturnsSumTimesLength()
   {
-    int[] region = [2, 1, 1, 2, 2, 1, 0, 1, 1, 1, 3, 3];
+    Cell[] region = [
+      (0, 0, [Border.Left, Border.Top]),
+      (1, 0, [Border.Top]),
+      (2, 0, [Border.Top]),
+      (3, 0, [Border.Top, Border.Right]),
+      (0, 1, [Border.Left, Border.Bottom]),
+      (1, 1, [Border.Bottom]),
+      (2, 1, []),
+      (3, 1, [Border.Right]),
+      (2, 2, [Border.Left]),
+      (3, 2, [Border.Bottom]),
+      (4, 2, [Border.Top, Border.Right, Border.Bottom]),
+      (2, 3, [Border.Right, Border.Bottom, Border.Left])
+    ];
     
     var result = Aoc2024Day12Processor.CalculateRegionValue(region);
     
     Assert.Equal(216, result);
+  }
+
+  [Fact]
+  public void CalculateDiscountedRegionValue_GivenRegion_ReturnsSidesTimesLength()
+  {
+    Cell[] region = [
+      (0, 0, [Border.Left, Border.Top]),
+      (1, 0, [Border.Top]),
+      (2, 0, [Border.Top]),
+      (3, 0, [Border.Top, Border.Right]),
+      (0, 1, [Border.Left, Border.Bottom]),
+      (1, 1, [Border.Bottom]),
+      (2, 1, []),
+      (3, 1, [Border.Right]),
+      (2, 2, [Border.Left]),
+      (3, 2, [Border.Bottom]),
+      (4, 2, [Border.Top, Border.Right, Border.Bottom]),
+      (2, 3, [Border.Right, Border.Bottom, Border.Left])
+    ];
+    
+    var result = Aoc2024Day12Processor.CalculateDiscountedRegionValue(region);
+    
+    Assert.Equal(120, result);
+  }
+
+  [Fact]
+  public void GetSides_GivenRegion_ReturnsNumberOfSides()
+  {
+    Cell[] region = [
+      (0, 0, [Border.Left, Border.Top]),
+      (1, 0, [Border.Top]),
+      (2, 0, [Border.Top]),
+      (3, 0, [Border.Top, Border.Right]),
+      (0, 1, [Border.Left, Border.Bottom]),
+      (1, 1, [Border.Bottom]),
+      (2, 1, []),
+      (3, 1, [Border.Right]),
+      (2, 2, [Border.Left]),
+      (3, 2, [Border.Bottom]),
+      (4, 2, [Border.Top, Border.Right, Border.Bottom]),
+      (2, 3, [Border.Right, Border.Bottom, Border.Left])
+    ];
+    
+    var result = Aoc2024Day12Processor.GetSides(region);
+    
+    Assert.Equal(10, result);
   }
   
   [Theory]
@@ -85,14 +145,19 @@ public class Aoc2024Day12ProcessorTest : ChallengeProcessorTests
 
     Assert.Equal(expectedResult, result);
   }
-
-  [Fact]
-  public void ProcessPart2Solution_GivenSampleInputs_ReturnsProvidedResult()
+  
+  [Theory]
+  [InlineData(1,"1206")]
+  [InlineData(2,"436")]
+  [InlineData(3,"80")]
+  [InlineData(4,"236")]
+  [InlineData(5,"368")]
+  public void ProcessPart2Solution_GivenSampleInputs_ReturnsProvidedResult(byte sample, string expectedResult)
   {
-    var input = GetSampleInput(1);
+    var input = GetSampleInput(sample);
 
     var result = _processor.ProcessPart2Solution(input);
 
-    Assert.Equal("???", result);
+    Assert.Equal(expectedResult, result);
   }
 }
--- AdventOfCode.App/Challenges/2024/Day12Processor.cs
+++ AdventOfCode.App/Challenges/2024/Day12Processor.cs
@@ -1,22 +1,30 @@
 using AdventOfCode.App.Core;
 using AdventOfCode.App.Utilities;
-using Cell = (int X, int Y, int Perimeter);
+using Cell = (int X, int Y, AdventOfCode.App.Challenges.Border[] Perimeter);
 
 namespace AdventOfCode.App.Challenges;
+
+public enum Border
+{
+  Top,
+  Left,
+  Bottom,
+  Right
+}
 
 [ChallengeProcessor(2024, 12)]
 public class Aoc2024Day12Processor : IChallengeProcessor
 {
-  public static int GetPerimeter(char[][] grid, char c, int x, int y)
+  public static Border[] GetPerimeter(char[][] grid, char c, int x, int y)
   {
-    var perimeters = 0;
+    List<Border> perimeters = [];
 
-    if (x <= 0 || grid[y][x - 1] != c) perimeters++;
-    if (x + 1 >= grid[y].Length || grid[y][x + 1] != c) perimeters++;
-    if (y <= 0 || grid[y - 1][x] != c) perimeters++;
-    if (y + 1 >= grid.Length || grid[y + 1][x] != c) perimeters++;
+    if (x <= 0 || grid[y][x - 1] != c) perimeters.Add(Border.Left);
+    if (x + 1 >= grid[y].Length || grid[y][x + 1] != c) perimeters.Add(Border.Right);
+    if (y <= 0 || grid[y - 1][x] != c) perimeters.Add(Border.Top);
+    if (y + 1 >= grid.Length || grid[y + 1][x] != c) perimeters.Add(Border.Bottom);
 
-    return perimeters;
+    return perimeters.ToArray();
   }
 
   // I will refactor this later™️
@@ -33,28 +41,28 @@ public class Aoc2024Day12Processor : IChallengeProcessor
 
       if (!cells.Any(cell => cell.X == x - 1 && cell.Y == y) &&
           !queue.Any(cell => cell.X == x - 1 && cell.Y == y) &&
-          x > 0 && grid[y][x - 1] == c)
+          !center.Perimeter.Contains(Border.Left))
       {
         queue.Enqueue((x - 1, y, GetPerimeter(grid, c, x - 1, y)));
       }
 
       if (!cells.Any(cell => cell.X == x + 1 && cell.Y == y) &&
           !queue.Any(cell => cell.X == x + 1 && cell.Y == y) &&
-          x + 1 < grid[y].Length && grid[y][x + 1] == c)
+          !center.Perimeter.Contains(Border.Right))
       {
         queue.Enqueue((x + 1, y, GetPerimeter(grid, c, x + 1, y)));
       }
 
       if (!cells.Any(cell => cell.X == x && cell.Y == y - 1) &&
           !queue.Any(cell => cell.X == x && cell.Y == y - 1) &&
-          y > 0 && grid[y - 1][x] == c)
+          !center.Perimeter.Contains(Border.Top))
       {
         queue.Enqueue((x, y - 1, GetPerimeter(grid, c, x, y - 1)));
       }
 
       if (!cells.Any(cell => cell.X == x && cell.Y == y + 1) &&
           !queue.Any(cell => cell.X == x && cell.Y == y + 1) &&
-          y + 1 < grid.Length && grid[y + 1][x] == c)
+          !center.Perimeter.Contains(Border.Bottom))
       {
         queue.Enqueue((x, y + 1, GetPerimeter(grid, c, x, y + 1)));
       }
@@ -81,10 +89,53 @@ public class Aoc2024Day12Processor : IChallengeProcessor
     return regions;
   }
 
-  public static int CalculateRegionValue(IEnumerable<int> input)
+  public static int CalculateRegionValue(IEnumerable<Cell> input)
   {
-    var region = input as int[] ?? input.ToArray();
-    return region.Sum() * region.Length;
+    var region = input as Cell[] ?? input.ToArray();
+    return region.Sum(e => e.Perimeter.Count()) * region.Length;
+  }
+
+
+  public static int CountBorders(Cell[] region, Border side)
+  {
+    var isHorizontal = side is Border.Top or Border.Bottom;
+    var groups = region
+      .Where(c => c.Perimeter.Contains(side))
+      .GroupBy(c => isHorizontal ? c.Y : c.X)
+      .Select(g => g.Select(e => isHorizontal ? e.X : e.Y));
+
+    var count = 0;
+
+    foreach (var group in groups)
+    {
+      var last = -1;
+      foreach (var b in group.Order())
+      {
+        if (last == -1 || b - last > 1)
+        {
+          count++;
+        }
+
+        last = b;
+      }
+    }
+
+    return count;
+  }
+
+  public static int GetSides(Cell[] region)
+  {
+    var total = CountBorders(region, Border.Top)
+                + CountBorders(region, Border.Bottom)
+                + CountBorders(region, Border.Left)
+                + CountBorders(region, Border.Right);
+    return total;
+  }
+
+  public static int CalculateDiscountedRegionValue(IEnumerable<Cell> input)
+  {
+    var region = input as Cell[] ?? input.ToArray();
+    return GetSides(region) * region.Length;
   }
 
   public string ProcessPart1Solution(string input)
@@ -92,11 +143,14 @@ public class Aoc2024Day12Processor : IChallengeProcessor
     var grid = input.ToCharGrid();
     var regions = ProcessRegions(grid);
 
-    return regions.Select(r => CalculateRegionValue(r.Select(e => e.Perimeter))).Sum().ToString();
+    return regions.Select(CalculateRegionValue).Sum().ToString();
   }
 
   public string ProcessPart2Solution(string input)
   {
-    throw new NotImplementedException();
+    var grid = input.ToCharGrid();
+    var regions = ProcessRegions(grid);
+
+    return regions.Select(CalculateDiscountedRegionValue).Sum().ToString();
   }
 }
