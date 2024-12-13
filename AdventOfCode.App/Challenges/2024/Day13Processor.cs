fb33395e9bb0b773813879ee048ad075fde2ee5c
Quangdao Nguyen <qnguyen@nvisia.com>
2024-12-13 09:07:46 -0600

    Set up 2024 Day 13

---
+++ AdventOfCode.App/Challenges/2024/Day13Processor.cs
@@ -0,0 +1,17 @@
+using AdventOfCode.App.Core;
+
+namespace AdventOfCode.App.Challenges;
+
+[ChallengeProcessor(2024, 13)]
+public class Aoc2024Day13Processor : IChallengeProcessor
+{
+  public string ProcessPart1Solution(string input)
+  {
+    throw new NotImplementedException();
+  }
+
+  public string ProcessPart2Solution(string input)
+  {
+    throw new NotImplementedException();
+  }
+}
