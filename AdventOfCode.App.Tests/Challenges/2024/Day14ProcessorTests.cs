97bd7961aedff2f1640e9faec1e64351cc3e6f0f
quangdaon <me@quangdao.com>
2024-12-14 10:23:28 -0600

    24D14 - Part 1

--- AdventOfCode.App.Tests/Challenges/2024/Day14ProcessorTests.cs
+++ AdventOfCode.App.Tests/Challenges/2024/Day14ProcessorTests.cs
@@ -1,4 +1,6 @@
+using System;
 using AdventOfCode.App.Challenges;
+using AdventOfCode.App.Utilities;
 using Xunit;
 
 namespace AdventOfCode.App.Tests.Challenges;
@@ -7,10 +9,45 @@ public class Aoc2024Day14ProcessorTest : ChallengeProcessorTests
 {
   private readonly Aoc2024Day14Processor _processor;
 
+  private const int WIDTH = 11;
+  private const int HEIGHT = 7;
+
   public Aoc2024Day14ProcessorTest() : base(2024, 14)
   {
     _processor = new Aoc2024Day14Processor();
-    _processor.SetSize(11, 7);
+    _processor.SetSize(WIDTH, HEIGHT);
+  }
+
+  [Theory]
+  [InlineData(0, 0, 4, 3, -3)]
+  [InlineData(1, 6, 3, -1, -3)]
+  public void RobotParse_GivenInputRow_ReturnDefinedRobot(int index, int expectedStartX, int expectedStartY,
+    int expectedVelocityX, int expectedVelocityY)
+  {
+    var input = GetSampleInput();
+    var rows = input.ToRows();
+
+    var row = rows[index];
+    var result = Aoc2024Day14Processor.Robot.Parse(row, WIDTH, HEIGHT);
+
+    Assert.Equal(expectedStartX, result.Start.X);
+    Assert.Equal(expectedStartY, result.Start.Y);
+    Assert.Equal(expectedVelocityX, result.Velocity.X);
+    Assert.Equal(expectedVelocityY, result.Velocity.Y);
+  }
+
+  [Theory]
+  [InlineData(0, 2, 4)]
+  [InlineData(1, 4, 1)]
+  [InlineData(2, 6, 5)]
+  [InlineData(3, 8, 2)]
+  public void ProjectPosition_GivenTime_ReturnsProjectedPosition(int time, int expectedX, int expectedY)
+  {
+    var robot = new Aoc2024Day14Processor.Robot((2, 4), (2, -3), WIDTH, HEIGHT);
+    var result = robot.ProjectPosition(time);
+
+    Assert.Equal(expectedX, result.X);
+    Assert.Equal(expectedY, result.Y);
   }
 
   [Fact]
@@ -23,7 +60,7 @@ public class Aoc2024Day14ProcessorTest : ChallengeProcessorTests
     Assert.Equal("12", result);
   }
 
-  [Fact]
+  [Fact(Skip = "Part 2 is Untestable")]
   public void ProcessPart2Solution_GivenSampleInputs_ReturnsProvidedResult()
   {
     var input = GetSampleInput();
--- AdventOfCode.App/Challenges/2024/Day14Processor.cs
+++ AdventOfCode.App/Challenges/2024/Day14Processor.cs
@@ -1,10 +1,12 @@
 using AdventOfCode.App.Core;
+using AdventOfCode.App.Utilities;
 
 namespace AdventOfCode.App.Challenges;
 
 [ChallengeProcessor(2024, 14)]
 public class Aoc2024Day14Processor : IChallengeProcessor
 {
+  private const int TIME_SKIP = 100;
   private int _width = 101;
   private int _height = 103;
 
@@ -13,10 +15,77 @@ public class Aoc2024Day14Processor : IChallengeProcessor
     _width = width;
     _height = height;
   }
-  
+
+  public class Robot
+  {
+    private readonly int _gridWidth;
+    private readonly int _gridHeight;
+
+    public Coordinates Start { get; set; }
+    public Coordinates Velocity { get; set; }
+
+    public Robot(Coordinates start, Coordinates velocity, int gridWidth, int gridHeight)
+    {
+      Start = start;
+      Velocity = velocity;
+      _gridWidth = gridWidth;
+      _gridHeight = gridHeight;
+    }
+
+    public Coordinates ProjectPosition(int time)
+    {
+      var x = (Start.X + (_gridWidth + Velocity.X) * time) % _gridWidth;
+      var y = (Start.Y + (_gridHeight + Velocity.Y) * time) % _gridHeight;
+      return (x, y);
+    }
+
+    public static Robot Parse(string row, int width, int height)
+    {
+      var components = row.Replace("p=", string.Empty).Split(" v=");
+      var p = components[0].Split(',').Select(int.Parse).ToArray();
+      var v = components[1].Split(',').Select(int.Parse).ToArray();
+
+      return new Robot((p[0], p[1]), (v[0], v[1]), width, height);
+    }
+  }
+
+  private int CalculateSafety(IEnumerable<Robot> robots)
+  {
+    var coords = robots.Select(r => r.ProjectPosition(TIME_SKIP));
+
+    var quadNW = 0;
+    var quadNE = 0;
+    var quadSW = 0;
+    var quadSE = 0;
+
+    var centerX = _width / 2;
+    var centerY = _height / 2;
+
+
+    foreach (var (x, y) in coords)
+    {
+      if (x < centerX)
+      {
+        if (y < centerY) quadNW++;
+        if (y > centerY) quadSW++;
+      }
+
+      if (x > centerX)
+      {
+        if (y < centerY) quadNE++;
+        if (y > centerY) quadSE++;
+      }
+    }
+    
+    return quadNW * quadNE * quadSW * quadSE;
+  }
+
   public string ProcessPart1Solution(string input)
   {
-    throw new NotImplementedException();
+    var rows = input.ToRows();
+    var robots = rows.Select(r => Robot.Parse(r, _width, _height));
+
+    return CalculateSafety(robots).ToString();
   }
 
   public string ProcessPart2Solution(string input)
